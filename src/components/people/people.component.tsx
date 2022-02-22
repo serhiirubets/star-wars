8fecf19a3fd859e1e6c8650e6ebac5404e17698a
serhiirubets <sergey.rubets@wnwd.com>
2022-02-22 21:58:01 +0200

    Add back button

--- src/components/people/people.component.tsx
+++ src/components/people/people.component.tsx
@@ -1,6 +1,6 @@
-import {Card, Col, Row} from 'antd';
+import {Button, Card, Col, Row} from 'antd';
 import {People} from '../peoples/peoples.types';
-import {useParams} from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import {useAppSelector} from '../../app/hooks';
 import {selectPeople} from '../peoples/peoples.selectors';
 import {useEffect} from 'react';
@@ -21,34 +21,40 @@ export const PeopleComponent = () => {
   }
 
   return (
-    <Row justify="center">
-      <Col>
-        <Card
-          style={{width: 400}}
-          cover={<img alt="example" src="https://via.placeholder.com/400.png/09f/fff"/>}
-        >
-          <Row justify="start">
-            <Col span={12}>Name:</Col>
-            <Col span={12}>{people.name}</Col>
-          </Row>
-          <Row justify="start">
-            <Col span={12}>Height:</Col>
-            <Col span={12}>{people.height}</Col>
-          </Row>
-          <Row justify="start">
-            <Col span={12}>Hair Color:</Col>
-            <Col span={12}>{people.hairColor}</Col>
-          </Row>
-          <Row justify="start">
-            <Col span={12}>Skin Color:</Col>
-            <Col span={12}>{people.skinColor}</Col>
-          </Row>
-          <Row justify="start">
-            <Col span={12}>Eye Color:</Col>
-            <Col span={12}>{people.eyeColor}</Col>
-          </Row>
-        </Card>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col offset={2}><Button type="primary" size="middle"><Link to={`/`}>Back</Link></Button></Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <Card
+            style={{width: 400}}
+            cover={<img alt="example" src="https://via.placeholder.com/400.png/09f/fff"/>}
+          >
+            <Row justify="start">
+              <Col span={12}>Name:</Col>
+              <Col span={12}>{people.name}</Col>
+            </Row>
+            <Row justify="start">
+              <Col span={12}>Height:</Col>
+              <Col span={12}>{people.height}</Col>
+            </Row>
+            <Row justify="start">
+              <Col span={12}>Hair Color:</Col>
+              <Col span={12}>{people.hairColor}</Col>
+            </Row>
+            <Row justify="start">
+              <Col span={12}>Skin Color:</Col>
+              <Col span={12}>{people.skinColor}</Col>
+            </Row>
+            <Row justify="start">
+              <Col span={12}>Eye Color:</Col>
+              <Col span={12}>{people.eyeColor}</Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </>
+
   )
 }
